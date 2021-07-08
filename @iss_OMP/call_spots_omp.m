c6a324db60a6868e98ce24b797e16e7810feed53
Josh Duffield <jduffield65@gmail.com>
2021-07-08 07:54:04 +0100

    Update call_spots_omp.m
    save iss object before get_omp_score

--- @iss_OMP/call_spots_omp.m
+++ @iss_OMP/call_spots_omp.m
@@ -159,6 +159,7 @@ o.ompCoefs = cell2mat(Coefs);
 o.ompNeighbNonZeros = cell2mat(NeighbNonZeros);
 [~,o.ompSpotIntensity2] = ...
     o.get_spot_intensity(o.ompSpotCodeNo,o.ompSpotColors,o.z_scoreSCALE);
+save(fullfile(o.OutputDirectory, 'oOMP'), 'o', '-v7.3');
 o.ompSpotScore = o.get_omp_score;
 end
 
